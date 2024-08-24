5e00d75c5dc769bded116e6af803b9a716c0c97d
metoebes <maryellen@toebes.com>
2024-08-23 22:30:32 -0400

    Minimal program to test android studio to robot controller setup is working.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/HelloWorld.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Hello World!", group = "Tutorials")
+public class HelloWorld  extends AbstractAutonomous{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // enable FTC dashboard integration
+        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        // Write a message out to the drive station
+        telemetry.addLine("Hello World !");
+        telemetry.update();
+
+        waitForStart();
+        telemetry.addLine("My name is _________");
+        telemetry.update();
+
+        // wait for end of autonomous
+        while(opModeIsActive()) {
+            idle();
+        }
+    }
+}
