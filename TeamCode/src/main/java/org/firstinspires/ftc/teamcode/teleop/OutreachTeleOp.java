c6a0c5c60d929b13e36c88e6d88db95341307d9a
martink27431 <martink27431@student.cghsnc.org>
2024-08-31 11:41:37 -0400

    Made Outreach TeleOp go forward and backward.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OutreachTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/OutreachTeleOp.java
@@ -1,18 +1,33 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.hardware.ImprovedGamepad;
 import org.firstinspires.ftc.teamcode.hardware.OutreachHardware;
 
 public class OutreachTeleOp extends OpMode {
     public OutreachHardware robot = new OutreachHardware();
+    public ImprovedGamepad gamepad;
     @Override
     public void init() {
         robot.init(this.hardwareMap);
+        gamepad = new ImprovedGamepad(this.gamepad1, new ElapsedTime(), "gamepad");
+        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
     public void loop() {
+        gamepad.update();
+        double leftMotor = gamepad.left_stick_y.getValue();
+        double rightMotor = gamepad.left_stick_y.getValue();
+        robot.leftDrive.setPower(leftMotor);
+        robot.rightDrive.setPower(rightMotor);
+        telemetry.addData("leftMotor", leftMotor);
+        telemetry.addData("rightMotor", rightMotor);
+
 
     }
 }
