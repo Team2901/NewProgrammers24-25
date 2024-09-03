efefda0f4aee4bd727ce2ec04021d3850ba1b2f2
martink27431 <martink27431@student.cghsnc.org>
2024-09-03 15:07:54 -0400

    Made Outreach TeleOp turn

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/KaylaMartinFirstAuto.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Kayla Martin First Auto", group = "Tutorial")
+public class KaylaMartinFirstAuto extends AbstractAutonomous {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(this.hardwareMap);
+        waitForStart();
+
+        for(int i = 0; i < 4; i++){
+            move(5);
+            turnLeft(3.5F);
+        }
+        for(int i = 0; i < 4; i++){
+            move(5);
+            turnRight(3.5F);
+        }
+
+
+    }
+
+}
