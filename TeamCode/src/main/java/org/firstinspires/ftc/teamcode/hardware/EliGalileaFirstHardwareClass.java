20724a92aa99dea701eb75e3e11d59c4eb4bbcb2
galileae28268 <galileae28268@student.cghsnc.org>
2024-11-05 15:58:25 -0500

    added on to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/EliGalileaFirstHardwareClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/EliGalileaFirstHardwareClass.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -10,12 +11,27 @@ public class EliGalileaFirstHardwareClass {
 
     public DcMotorEx rightMove;
 
+    public DcMotorEx arm;
+
     public Servo claw;
+
+    public enum ArmState {
+        HIGH,
+        MID,
+        LOW
+    }
+
+    public static final double CLAW_OPEN_POSITION = 0.2;
+    public static final double CLAW_CLOSED_POSITION = 0.48;
+    public AnalogInput potentiometer;
 
     public void init(HardwareMap hardwareMap) {
         leftMove = hardwareMap.get(DcMotorEx.class, "leftDrive");
         rightMove = hardwareMap.get(DcMotorEx.class, "rightDrive");
         claw = hardwareMap.servo.get("claw");
+        claw.setPosition(CLAW_CLOSED_POSITION);
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
+        potentiometer = hardwareMap.analogInput.get("potentiometer");
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/EliGalileaFirstTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/EliGalileaFirstTeleop.java
@@ -12,6 +12,8 @@ import org.firstinspires.ftc.teamcode.hardware.UpdatedClawbotHardware;
 public class EliGalileaFirstTeleop extends OpMode {
     EliGalileaFirstHardwareClass robot = new EliGalileaFirstHardwareClass();
     ImprovedGamepad gamepad;
+    boolean isClosed = true;
+    EliGalileaFirstHardwareClass.ArmState armState = EliGalileaFirstHardwareClass.ArmState.LOW;
 
     @Override
     public void init() {
@@ -22,27 +24,57 @@ public class EliGalileaFirstTeleop extends OpMode {
     @Override
     public void loop() {
         gamepad.update();
+
         robot.leftMove.setPower(gamepad.left_stick_y.getValue());
-        robot.rightMove.setPower(gamepad.right_stick_y.getValue());
+        robot.rightMove.setPower(-gamepad.right_stick_y.getValue());
 
-        telemetry.addData("left stick y", gamepad.left_stick_y.getValue());
-        telemetry.addData("right stick y", gamepad.right_stick_y.getValue());
-
-        if(gamepad.dpad_up.getValue() == true){
-            robot.rightMove.setPower(0.5);
-            robot.leftMove.setPower(0.5);
-
+        if(isClosed == true){
+            if (gamepad.b.isInitialPress()) {
+                isClosed = false;
+                robot.claw.setPosition(EliGalileaFirstHardwareClass.CLAW_OPEN_POSITION);
+            }
         } else {
-            robot.rightMove.setPower(0);
-            robot.leftMove.setPower(0);
+
+            if (gamepad.b.isInitialPress()) {
+                robot.claw.setPosition(EliGalileaFirstHardwareClass.CLAW_CLOSED_POSITION);
+                isClosed = true;
+            }
         }
 
-        if(gamepad.dpad_down.getValue()){
-            robot.rightMove.setPower(-0.5);
-            robot.leftMove.setPower(-0.5);
-        } else {
-            robot.rightMove.setPower(0);
-            robot.leftMove.setPower(0);
+        if (armState == EliGalileaFirstHardwareClass.ArmState.LOW) {
+            if (robot.potentiometer.getVoltage() > 3.061) {
+                robot.arm.setPower(0.5);
+            } else {
+                robot.arm.setPower(0);
+            }
         }
+
+
+        // low pos at 3.261
+        // mid pos at 2.133
+        // high pos at 1.29
+
+//        robot.leftMove.setPower(gamepad.left_stick_y.getValue());
+//        robot.rightMove.setPower(gamepad.right_stick_y.getValue());
+//
+//        telemetry.addData("left stick y", gamepad.left_stick_y.getValue());
+//        telemetry.addData("right stick y", gamepad.right_stick_y.getValue());
+//
+//        if(gamepad.dpad_up.getValue() == true){
+//            robot.rightMove.setPower(0.5);
+//            robot.leftMove.setPower(0.5);
+//
+//        } else {
+//            robot.rightMove.setPower(0);
+//            robot.leftMove.setPower(0);
+//        }
+//
+//        if(gamepad.dpad_down.getValue()){
+//            robot.rightMove.setPower(-0.5);
+//            robot.leftMove.setPower(-0.5);
+//        } else {
+//            robot.rightMove.setPower(0);
+//            robot.leftMove.setPower(0);
+//        }
     }
 }
